2bb2f18babd80a0cd8074c7f302f83c7d7b76f90
Pranav Vashi <neobuddy89@gmail.com>
2019-07-07 11:56:32 +0530

    Fix long click intent for Smart Pixels tile [2/2]
    Change-Id: Id6b5fffd814f035be1bb75d1434456e0c02aa97f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -197,6 +197,7 @@ import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSetting
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.power.hub.powerhub;
+import com.power.hub.fragments.SmartPixels;
 
 public class SettingsGateway {
 
@@ -383,6 +384,7 @@ public class SettingsGateway {
             UserAspectRatioDetails.class.getName(),
             powerhub.class.getName(),
             RunningServices.class.getName(),
+            SmartPixels.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -431,5 +433,6 @@ public class SettingsGateway {
             Settings.NavigationModeSettingsActivity.class.getName(),
             Settings.powerhubActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
+            Settings.SmartPixelsActivity.class.getName(),
     };
 }
