18d699514ac0b6921ebf94ca217a597b61ebfedd
Jackeagle <jackeagle102@gmail.com>
2020-06-25 10:52:51 +0200

    Settings: Switch to string overlay for maintainer string
    - Define following overlay in your device tree
        <string name="voltage_maintainer"></string>
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: I65ad87ee20547320c97f1f0261d988307744b89f
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/deviceinfo/voltage/VoltageMaintainerPreferenceController.java
+++ src/com/android/settings/deviceinfo/voltage/VoltageMaintainerPreferenceController.java
@@ -17,7 +17,7 @@
 package com.android.settings.deviceinfo.voltage;
 
 import android.content.Context;
-import android.os.SystemProperties;
+import android.content.res.Resources;
 import android.text.TextUtils;
 
 import androidx.preference.Preference;
@@ -28,7 +28,6 @@ import com.android.settings.core.BasePreferenceController;
 public class VoltageMaintainerPreferenceController extends BasePreferenceController {
 
     private static final String TAG = "VoltageMaintainerPreferenceController";
-    private static final String ROM_PROPERTY = "ro.voltage.maintainer";
 
     public VoltageMaintainerPreferenceController(Context context, String key) {
         super(context, key);
@@ -39,8 +38,7 @@ public class VoltageMaintainerPreferenceController extends BasePreferenceControl
     }
 
     public CharSequence getSummary() {
-        String rom = SystemProperties.get(ROM_PROPERTY,
-                this.mContext.getString(R.string.device_info_default));
-        return rom;
+        String maintainer = mContext.getResources().getString(R.string.voltage_maintainer);
+        return maintainer;
     }
 }
