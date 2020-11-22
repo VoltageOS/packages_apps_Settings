8f102ffe145bfc2b5ea2d795a7405d77c335c153
LuK1337 <priv.luk@gmail.com>
2020-11-22 03:15:07 +0100

    [SQUASH] Settings: introduce refresh rate preferences
    commit bf440f2b69dbd5c23b789adcab2226e972bf8768
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sun Nov 22 03:15:07 2020 +0100
    
        Settings: Add preference for KEY_MIN_REFRESH_RATE
    
        Change-Id: Iac1f65ab09717ea55a5b471e094385c77ba894ee
        Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    commit 9d08d502c7cac9b0afdc3165281b1116e743b51e
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Mon Jun 14 03:34:06 2021 +0300
    
        Settings: Add peak refresh rate list preference
    
        * AOSP "Smooth display" setting is just a toggle,
          some devices support multiple refresh rates so
          add support for it with a ListPreference.
    
        [jhonboy121]: unify config flag for both min and max list preference
    
        Change-Id: I3da3d2b86e61ed3caf9af5770d8bdb4485817b97
    
    commit fa56b3198185ddb011e6f20785487f5deb49f407
    Author: jhonboy121 <alfredmathew05@gmail.com>
    Date:   Fri Feb 18 01:49:38 2022 +0530
    
        Settings: rewrite refresh rate preference controllers in kt and improve
    
        * Added settings observer to update preference on settings changes. We have a tile so it would
          be nice to reflect changes when tile state is changed.
        * Restrict max refresh rate list based on the min rate set since it is absurd to allow setting max
          values below current min rate.
    
        Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    commit d2ea720849218b6fc4073ce11be21719d80d3cfd
    Author: Alfred Mathew <alfredmathew05@gmail.com>
    Date:   Fri Feb 18 14:49:48 2022 +0530
    
        Settings: display 60 as min refresh rate if key value is 0
    
        Signed-off-by: Alfred Mathew <alfredmathew05@gmail.com>
    
    commit 5ad60cf9454b6c9d1564031e544efa19f81f998f
    Author: jhonboy121 <alfredmathew05@gmail.com>
    Date:   Mon Feb 28 23:37:51 2022 +0530
    
        Settings: fix refresh rate list preference values order
    
        * basically just sort it before adding to list, apparently the order was not ascending for vayu
    
        Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -24,6 +24,8 @@ import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
+import com.android.settings.display.MaxRefreshRatePreferenceController;
+import com.android.settings.display.MinRefreshRatePreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
@@ -80,6 +82,8 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
+        controllers.add(new MinRefreshRatePreferenceController(context, lifecycle));
+        controllers.add(new MaxRefreshRatePreferenceController(context, lifecycle));
         return controllers;
     }
 
