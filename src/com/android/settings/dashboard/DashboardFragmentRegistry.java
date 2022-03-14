0c73aca416593048fdb7d668a5df49a23da3975f
maxwen <max.weninger@gmail.com>
2022-03-14 08:10:53 +0000

    Add Power Hub to dashboard
    Change-Id: I9c2a3108e37810634c3ef515ecc17504629bd433
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/dashboard/DashboardFragmentRegistry.java
+++ src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -52,6 +52,8 @@ import com.android.settings.supervision.SupervisionDashboardFragment;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
+import com.power.hub.powerhub;
+
 import java.util.Map;
 
 /**
@@ -138,6 +140,8 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_MORE_SECURITY_PRIVACY_SETTINGS);
         PARENT_TO_CATEGORY_KEY_MAP.put(SupervisionDashboardFragment.class.getName(),
                 CategoryKey.CATEGORY_SUPERVISION);
+        PARENT_TO_CATEGORY_KEY_MAP.put(powerhub.class.getName(),
+                CategoryKey.CATEGORY_SYSTEM_DEVELOPMENT);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 
