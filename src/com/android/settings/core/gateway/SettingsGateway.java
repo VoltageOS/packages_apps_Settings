bb038b4fb4e0e65c4c01445be66530df1d23a702
Anushek Prasal <anushekprasal@gmail.com>
2022-04-12 13:12:46 +0800

    Settings: Introduce automated Sleep Mode [2/2]
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -121,6 +121,7 @@ import com.android.settings.fuelgauge.batterysaver.BatterySaverScheduleSettings;
 import com.android.settings.fuelgauge.batterysaver.BatterySaverSettings;
 import com.android.settings.fuelgauge.batteryusage.PowerUsageAdvanced;
 import com.android.settings.fuelgauge.batteryusage.PowerUsageSummary;
+import com.android.settings.fuelgauge.sleepmode.SleepMode;
 import com.android.settings.gestures.ButtonNavigationSettingsFragment;
 import com.android.settings.gestures.DoubleTapPowerSettings;
 import com.android.settings.gestures.DoubleTapScreenSettings;
@@ -447,6 +448,7 @@ public class SettingsGateway {
             WifiTetherClientManager.class.getName(),
             LiveDisplaySettings.class.getName(),
             RunningServices.class.getName(),
+            SleepMode.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -498,5 +500,6 @@ public class SettingsGateway {
             Settings.powerhubActivity.class.getName(),
             Settings.ScreenRefreshRateActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
+            Settings.SleepModeActivity.class.getName()
     };
 }
