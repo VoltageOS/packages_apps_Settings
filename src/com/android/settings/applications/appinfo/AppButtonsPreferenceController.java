3e85617f2b1fd8a363bed334b4354f09d9d2b686
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-09-21 18:15:51 +0300

    don't allow disabling GmsCompat config
    Disabling it would prevent auto-updates via Apps app.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/applications/appinfo/AppButtonsPreferenceController.java
+++ src/com/android/settings/applications/appinfo/AppButtonsPreferenceController.java
@@ -627,6 +627,7 @@ public class AppButtonsPreferenceController extends BasePreferenceController imp
         if (mHomePackages.contains(mAppEntry.info.packageName)
                 || mAppEntry.info.packageName.equals("com.android.inputmethod.latin")
                 || mAppEntry.info.packageName.equals(GmsCompatApp.PKG_NAME)
+                || mAppEntry.info.packageName.equals(GmsCompatApp.PKG_NAME + ".config")
                 || isSystemPackage(mActivity.getResources(), mPm, mPackageInfo)) {
             // Disable button for core system applications.
             mButtonsPref.setButton2Text(R.string.disable_text)
