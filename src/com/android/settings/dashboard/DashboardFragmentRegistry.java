b14d1bda2e9db120a0e8385f164c96f3040291cc
Chirayu Desai <chirayudesai1@gmail.com>
2023-01-18 21:03:56 +0530

    Let "Other options" get injected
    ```
    <intent-filter>
               <action android:name="com.android.settings.action.IA_SETTINGS"/>
               <category android:name="android.intent.category.DEFAULT" />
    </intent-filter>
    
    <meta-data android:name="com.android.settings.category"
               android:value="com.android.settings.category.ia.other" />
    ```
    
    Test: Manual, attempted to inject BatteryStatsViewer and succeeded
    Change-Id: I9a347e21adb648fb4d57d2d3f0390334c741306e
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/dashboard/DashboardFragmentRegistry.java
+++ src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -41,6 +41,7 @@ import com.android.settings.homepage.TopLevelSettings;
 import com.android.settings.network.NetworkDashboardFragment;
 import com.android.settings.notification.ConfigureNotificationSettings;
 import com.android.settings.notification.SoundSettings;
+import com.android.settings.other.OtherFragment;
 import com.android.settings.privacy.PrivacyDashboardFragment;
 import com.android.settings.safetycenter.ExploitProtectionFragment;
 import com.android.settings.safetycenter.MoreSecurityPrivacyFragment;
@@ -145,6 +146,8 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_PRIVACY_CONTROLS);
         PARENT_TO_CATEGORY_KEY_MAP.put(powerhub.class.getName(),
                 CategoryKey.CATEGORY_SYSTEM_DEVELOPMENT);
+        PARENT_TO_CATEGORY_KEY_MAP.put(OtherFragment.class.getName(),
+                CategoryKey.CATEGORY_OTHER);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 
