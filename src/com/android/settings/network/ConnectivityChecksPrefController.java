6ff660e77457a5a077fe5a1a29ce312c36b84487
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-24 13:32:14 +0300

    add connectivity checks setting
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

---
+++ src/com/android/settings/network/ConnectivityChecksPrefController.java
@@ -0,0 +1,29 @@
+package com.android.settings.network;
+
+import android.content.Context;
+import android.ext.settings.ConnChecksSetting;
+
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.ext.IntSettingPrefController;
+import com.android.settings.ext.RadioButtonPickerFragment2;
+
+public class ConnectivityChecksPrefController extends IntSettingPrefController {
+
+    public ConnectivityChecksPrefController(Context ctx, String key) {
+        super(ctx, key, ConnChecksSetting.SYS_PROP);
+    }
+
+    @Override
+    protected void getEntries(Entries entries) {
+        entries.add(R.string.conn_checks_grapheneos_server, ConnChecksSetting.VAL_GRAPHENEOS);
+        entries.add(R.string.conn_checks_google_server, ConnChecksSetting.VAL_STANDARD);
+        entries.add(R.string.conn_checks_disabled, ConnChecksSetting.VAL_DISABLED);
+    }
+
+    @Override
+    public void addPrefsAfterList(RadioButtonPickerFragment2 fragment, PreferenceScreen screen) {
+        addFooterPreference(screen, R.string.conn_checks_footer);
+    }
+}
