018448bdc07ebe7c52982d56f012418e4485fb2f
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2023-07-01 07:07:57 +0000

    Remove "Add users from lock screen" setting
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/users/AddUserWhenLockedPreferenceController.java
+++ src/com/android/settings/users/AddUserWhenLockedPreferenceController.java
@@ -57,19 +57,7 @@ public class AddUserWhenLockedPreferenceController extends TogglePreferenceContr
 
     @Override
     public int getAvailabilityStatus() {
-        if (!mUserCaps.isAdmin()) {
-            return DISABLED_FOR_USER;
-        } else if (android.multiuser.Flags.newMultiuserSettingsUx()) {
-            if (mUserCaps.mDisallowAddUser && !mUserCaps.mDisallowAddUserSetByAdmin) {
-                return DISABLED_FOR_USER;
-            } else {
-                return AVAILABLE;
-            }
-        } else if (mUserCaps.disallowAddUser() || mUserCaps.disallowAddUserSetByAdmin()) {
-            return DISABLED_FOR_USER;
-        } else {
-            return mUserCaps.mUserSwitcherEnabled ? AVAILABLE : CONDITIONALLY_UNAVAILABLE;
-        }
+        return UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
