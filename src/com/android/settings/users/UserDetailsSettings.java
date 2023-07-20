76197b6e081a0dce684c8fb6bf060da8852c277b
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2023-07-20 05:07:49 +0000

    UserManager settings for apps installs and updates
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/users/UserDetailsSettings.java
+++ src/com/android/settings/users/UserDetailsSettings.java
@@ -79,6 +79,8 @@ public class UserDetailsSettings extends SettingsPreferenceFragment
     private static final String KEY_APP_AND_CONTENT_ACCESS = "app_and_content_access";
     private static final String KEY_APP_COPYING = "app_copying";
 
+    private static final String KEY_APP_INSTALLS = "app_installs";
+
     /** Integer extra containing the userId to manage */
     static final String EXTRA_USER_ID = "user_id";
 
@@ -114,6 +116,7 @@ public class UserDetailsSettings extends SettingsPreferenceFragment
     Preference mRemoveUserPref;
     @VisibleForTesting
     TwoStatePreference mGrantAdminPref;
+    Preference mAppsInstallsPref;
 
     @VisibleForTesting
     /** The user being studied (not the user doing the studying). */
@@ -150,6 +153,8 @@ public class UserDetailsSettings extends SettingsPreferenceFragment
         if (mUserInfo.isGuest() && mGuestUserAutoCreated) {
             mRemoveUserPref.setEnabled((mUserInfo.flags & UserInfo.FLAG_INITIALIZED) != 0);
         }
+        mAppsInstallsPref.setSummary(UserAppsInstallSettings.getDescription(
+                requireContext(), userRestrictions));
     }
 
     @Override
@@ -189,6 +194,9 @@ public class UserDetailsSettings extends SettingsPreferenceFragment
             return true;
         } else if (preference == mAppCopyingPref) {
             openAppCopyingScreen();
+            return true;
+        } else if (preference == mAppsInstallsPref) {
+            UserAppsInstallSettings.launch(preference, mUserInfo.id);
             return true;
         }
         return false;
@@ -376,6 +384,7 @@ public class UserDetailsSettings extends SettingsPreferenceFragment
         mGrantAdminPref = findPreference(KEY_GRANT_ADMIN);
 
         mGrantAdminPref.setChecked(mUserInfo.isAdmin());
+        mAppsInstallsPref = findPreference(KEY_APP_INSTALLS);
 
         mSwitchUserPref.setVisible(mUserCaps.mUserSwitchingUiEnabled
                 && UserHandle.myUserId() != mUserInfo.id);
@@ -432,6 +441,7 @@ public class UserDetailsSettings extends SettingsPreferenceFragment
             removePreference(KEY_ENABLE_TELEPHONY_CALLING);
             removePreference(KEY_APP_AND_CONTENT_ACCESS);
             removePreference(KEY_APP_COPYING);
+            removePreference(KEY_APP_INSTALLS);
         } else {
             if (!Utils.isVoiceCapable(context)) { // no telephony
                 removePreference(KEY_ENABLE_TELEPHONY_CALLING);
@@ -466,6 +476,7 @@ public class UserDetailsSettings extends SettingsPreferenceFragment
                 removePreference(KEY_REMOVE_USER);
             }
 
+            mAppsInstallsPref.setOnPreferenceClickListener(this);
             mPhonePref.setOnPreferenceChangeListener(this);
             mGrantAdminPref.setOnPreferenceChangeListener(this);
             mAppAndContentAccessPref.setOnPreferenceClickListener(this);
