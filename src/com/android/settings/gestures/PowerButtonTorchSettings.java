pixeldust-project-caf/android_packages_apps_Settings

Allow toggling torch by double/long pressing power button [2/2]. Combines the quick torch gesture options (double tap, long press) into one settings page.
Completely rewritten for Android 13 (tm-qpr1) using PowerMenuSettingsUtils.

For use with the fw/b commit authored by jhonboy121: https://github.com/Flamingo-OS/frameworks_base/commit/85a187dfc2b87c45f7e89cd3afc98fd65007d2db

Squashed-in:
* QuickTorch: Use new material-you illustration preference
* Switch to new Android 12 animation. Huge thanks to aryanata for the animation

Co-authored-by: Aryanata Andipradana <a.ryan.ata.ap@gmail.com>
Signed-off-by: spezi77 <spezi7713@gmx.net>

src/com/android/settings/gestures/PowerButtonTorchSettings.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2023 The PixelDust Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.gestures;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+
+public class PowerButtonTorchSettings extends DashboardFragment {
+
+    private static final String TAG = "PowerButtonTorchSettings";
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.PIXELDUST;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.power_button_torch;
+    }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.power_button_torch);
+}

VoltageOS/packages_apps_Settings

Signed-off-by: Dmitrii <bankersenator@gmail.com>

src/com/android/settings/gestures/PowerButtonTorchSettings.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2023 The PixelDust Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.gestures;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+
+public class PowerButtonTorchSettings extends DashboardFragment {
+
+    private static final String TAG = "PowerButtonTorchSettings";
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.VOLTAGE;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.power_button_torch;
+    }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.power_button_torch);
+}