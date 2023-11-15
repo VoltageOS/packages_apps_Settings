8a129578d75f486af9fb6edddf38a7555193dc69
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-11-15 13:44:23 +0200

    infrastructure for AppSwitch UIs
    Squashed with 28baa8dcbf5a0d285d8bf3852b723beaa290a973 by quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
+++ src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
@@ -46,6 +46,7 @@ import com.android.settings.spa.app.specialaccess.PictureInPictureListProvider
 import com.android.settings.spa.app.specialaccess.UsageDataAppListProvider
 import com.android.settings.spa.app.specialaccess.WriteSystemPreferencesAppListProvider
 import com.android.settingslib.spa.framework.common.SettingsPageProvider
+import com.android.settingslib.spa.framework.compose.LifecycleEffect
 import com.android.settingslib.spa.framework.compose.navigator
 import com.android.settingslib.spa.widget.scaffold.RegularScaffold
 import com.android.settingslib.spa.widget.ui.Category
@@ -121,6 +122,8 @@ object AppInfoSettingsProvider : SettingsPageProvider {
 
 @Composable
 private fun AppInfoSettings(packageInfoPresenter: PackageInfoPresenter) {
+    // needed to refresh AppSwitch items after returning from their fragments
+    LifecycleEffect(onStart = { check(packageInfoPresenter.manualRefreshFlow.tryEmit(Any())) })
     val packageInfoState = packageInfoPresenter.flow.collectAsStateWithLifecycle()
     val featureFlags: PmFeatureFlags = PmFeatureFlagsImpl()
     RegularScaffold(
