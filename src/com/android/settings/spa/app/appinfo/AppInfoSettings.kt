460f8e36a821eb1b59858cc5905cc845d9c45afe
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-03-02 11:25:46 +0200

    add logcat viewer link to App info screen
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
+++ src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
@@ -160,6 +160,7 @@ private fun AppInfoSettings(packageInfoPresenter: PackageInfoPresenter) {
             AppLocalePreference(app)
             AppOpenByDefaultPreference(app)
             DefaultAppShortcuts(app)
+            AppLogcatPreference(app)
         }
 
         Category(title = stringResource(R.string.unused_apps_category)) {
