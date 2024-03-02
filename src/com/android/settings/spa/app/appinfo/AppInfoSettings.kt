11befa8bdf0b4e6447c24d6ff97b8b466327a24e
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-03-02 11:19:46 +0200

    add Storage Scopes link to the "App info" screen
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
+++ src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
@@ -147,6 +147,7 @@ private fun AppInfoSettings(packageInfoPresenter: PackageInfoPresenter) {
             AppAllServicesPreference(app)
             AppNotificationPreference(app)
             AppPermissionPreference(app)
+            AppStorageScopesPreference(app)
             AppStoragePreference(app)
             InstantAppDomainsPreference(app)
             AppDataUsagePreference(app)
