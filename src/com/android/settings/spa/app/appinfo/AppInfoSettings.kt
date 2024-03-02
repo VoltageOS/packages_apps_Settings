91b8637ec9e29be00a3bd4d10665a4c193b2f4b4
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-03-02 12:09:48 +0200

    add "Exploit protection" category to App info
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
+++ src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
@@ -178,6 +178,9 @@ private fun AppInfoSettings(packageInfoPresenter: PackageInfoPresenter) {
             Enable16KbAppCompatPreference(app)
         }
 
+        Category(title = stringResource(R.string.exploit_protection_category_title)) {
+        }
+
         Category(title = stringResource(R.string.app_install_details_group_title)) {
             AppInstallerInfoPreference(app)
         }
