bb9e32db05582ebdabf128ed5386042605d68091
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-08-12 17:45:43 +0300

    add Private Space sandboxed Google Play link
    It's added to Private Space screen instead of the Apps screen (where regular and work profile links
    are) to avoid revealing whether the Private Space is present when the "Hide when locked" Private
    Space option is enabled.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/applications/GmsCompatAppController.java
+++ src/com/android/settings/applications/GmsCompatAppController.java
@@ -1,16 +1,19 @@
 package com.android.settings.applications;
 
+import android.annotation.Nullable;
 import android.app.compat.gms.GmsCompat;
 import android.content.Context;
 import android.content.Intent;
 import android.os.UserHandle;
 import android.text.TextUtils;
+import android.util.Log;
 
 import androidx.preference.Preference;
 
 import com.android.internal.gmscompat.GmsCompatApp;
 import com.android.internal.gmscompat.GmsInfo;
 import com.android.settings.core.BasePreferenceController;
+import com.android.settings.privatespace.PrivateSpaceMaintainer;
 
 public class GmsCompatAppController extends BasePreferenceController {
 
@@ -18,14 +21,28 @@ public class GmsCompatAppController extends BasePreferenceController {
         super(context, key);
     }
 
+    @Nullable
+    private UserHandle getUser() {
+        if ("sandboxed_google_play_private_space".equals(getPreferenceKey())) {
+            return PrivateSpaceMaintainer.getInstance(mContext).getPrivateProfileHandle();
+        }
+
+        UserHandle workProfileUser = getWorkProfileUser();
+        if (workProfileUser != null) {
+            return workProfileUser;
+        }
+
+        return mContext.getUser();
+    }
+
     @Override
     public int getAvailabilityStatus() {
-        UserHandle workProfile = getWorkProfileUser();
-        int userId = workProfile != null ?
-                workProfile.getIdentifier() :
-                UserHandle.myUserId();
+        UserHandle user = getUser();
+        if (user == null) {
+            return DISABLED_FOR_USER;
+        }
 
-        return GmsCompat.isEnabledFor(GmsInfo.PACKAGE_GMS_CORE, userId) ?
+        return GmsCompat.isEnabledFor(GmsInfo.PACKAGE_GMS_CORE, user.getIdentifier()) ?
                 AVAILABLE : DISABLED_FOR_USER;
     }
 
@@ -34,15 +51,15 @@ public class GmsCompatAppController extends BasePreferenceController {
         if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
             return false;
         }
+
+        UserHandle user = getUser();
+        if (user == null) {
+            throw new IllegalStateException("getUser() returned null, key: " + getPreferenceKey());
+        }
+
         Intent intent = new Intent(GmsCompatApp.PKG_NAME + ".SETTINGS_LINK");
         intent.setPackage(GmsCompatApp.PKG_NAME);
-
-        UserHandle workProfile = getWorkProfileUser();
-        if (workProfile != null) {
-            mContext.startActivityAsUser(intent, workProfile);
-        } else {
-            mContext.startActivity(intent);
-        }
+        mContext.startActivityAsUser(intent, user);
         return true;
     }
 }
