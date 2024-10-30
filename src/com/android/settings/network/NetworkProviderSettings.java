ffc897c0b5a1493148a570b3df72f187fe4ec9be
Alexander Koskovich <akoskovich@pm.me>
2024-10-30 17:49:38 -0400

    Add a mobile data toggle for setup
    mIsInSetupWizard was moved to top of NetworkProviderSetting's onCreate
    so it is available for addPreferences().
    
    Change-Id: I9299c3033e96bcd30450ec239f14fb5daa800d63
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/network/NetworkProviderSettings.java
+++ src/com/android/settings/network/NetworkProviderSettings.java
@@ -81,6 +81,7 @@ import com.android.settings.network.ethernet.EthernetSwitchPreferenceController;
 import com.android.settings.network.ethernet.EthernetTracker;
 import com.android.settings.network.ethernet.EthernetTrackerImpl;
 import com.android.settings.network.MobileDataEnabledListener;
+import com.android.settings.network.telephony.MobileDataPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.widget.GearPreference;
 import com.android.settings.wifi.AddNetworkFragment;
@@ -282,6 +283,12 @@ public class NetworkProviderSettings extends RestrictedDashboardFragment
     PreferenceCategory mEthernetPreferenceCategory;
 
     /**
+     * Mobile data toggle
+     */
+    private static final String PREF_KEY_MOBILE_DATA_TOGGLE = "main_toggle_mobile_data";
+    private MobileDataPreferenceController mMobileDataPreferenceController;
+
+    /**
      * Mobile networks list for provider model
      */
     private static final String PREF_KEY_PROVIDER_MOBILE_NETWORK = "provider_model_mobile_network";
@@ -340,6 +347,7 @@ public class NetworkProviderSettings extends RestrictedDashboardFragment
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         final Context context = getContext();
+        final Intent intent = this.getIntent();
         if (context != null && !context.getResources().getBoolean(
                 R.bool.config_show_internet_settings)) {
             finish();
@@ -347,6 +355,7 @@ public class NetworkProviderSettings extends RestrictedDashboardFragment
         }
         mAirplaneModeEnabler = new AirplaneModeEnabler(getContext(), this);
         mDataStateListener = new MobileDataEnabledListener(getContext(), this);
+        mIsInSetupWizard = WizardManagerHelper.isAnySetupWizard(intent);
 
         // TODO(b/37429702): Add animations and preference comparator back after initial screen is
         // loaded (ODR).
@@ -394,8 +403,6 @@ public class NetworkProviderSettings extends RestrictedDashboardFragment
                 fixConnectivityItem.setVisible(!mIsGuest && (!isAirplaneModeOn || isWifiEnabled));
             }
         };
-        final Intent intent = this.getIntent();
-        mIsInSetupWizard = WizardManagerHelper.isAnySetupWizard(intent);
     }
 
     private void updateUserType() {
@@ -448,6 +455,7 @@ public class NetworkProviderSettings extends RestrictedDashboardFragment
         if (mResetInternetPreference != null) {
             mResetInternetPreference.setVisible(false);
         }
+        addMobileDataPreferenceController();
         addNetworkMobileProviderController();
         addWifiSwitchPreferenceController();
         if (com.android.settings.connectivity.Flags.ethernetSettings()) {
@@ -473,6 +481,18 @@ public class NetworkProviderSettings extends RestrictedDashboardFragment
     boolean showAnySubscriptionInfo(Context context) {
         return (context != null) && (Utils.isMobileDataCapable(context)
                 || Utils.isVoiceCapable(context));
+    }
+
+    private void addMobileDataPreferenceController() {
+        if (!showAnySubscriptionInfo(getContext())) {
+            return;
+        }
+        if (mMobileDataPreferenceController == null) {
+            mMobileDataPreferenceController = new MobileDataPreferenceController(
+                    getContext(), PREF_KEY_MOBILE_DATA_TOGGLE, getSettingsLifecycle(),
+                    mSubId, mIsInSetupWizard);
+        }
+        mMobileDataPreferenceController.displayPreference(getPreferenceScreen());
     }
 
     private void addNetworkMobileProviderController() {
