cdd75f92e428c88d65d311f2faf27d82d0166823
Dmitrii <bankersenator@gmail.com>
2024-11-21 18:59:48 -0500

    Settings: simplify version string
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/VoltageVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/VoltageVersionPreferenceController.java
@@ -42,7 +42,7 @@ public class VoltageVersionPreferenceController extends BasePreferenceController
     private static final int DELAY_TIMER_MILLIS = 500;
     private static final int ACTIVITY_TRIGGER_COUNT = 3;
 
-    private static final String KEY_VOLTAGE_VERSION_PROP = "ro.voltage.version";
+    private static final String KEY_VOLTAGE_VERSION_PROP = "org.voltage.version";
 
     private final UserManager mUserManager;
     private final long[] mHits = new long[ACTIVITY_TRIGGER_COUNT];
