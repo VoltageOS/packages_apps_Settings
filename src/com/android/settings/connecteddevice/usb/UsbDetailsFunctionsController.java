c76d43ecec093e703fc3185a53f4120403323f55
Ido Ben-Hur <idoybh2@gmail.com>
2025-01-09 17:24:27 +0200

    Settings: Skip auth for USB func ctrl when unlocked in the last 60s
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/connecteddevice/usb/UsbDetailsFunctionsController.java
+++ src/com/android/settings/connecteddevice/usb/UsbDetailsFunctionsController.java
@@ -35,6 +35,7 @@ import androidx.preference.PreferenceScreen;
 import com.android.settings.R;
 import com.android.settings.Utils;
 import com.android.settingslib.RestrictedSelectorWithWidgetPreference;
+import com.android.settings.wifi.dpp.WifiDppUtils;
 import com.android.settingslib.widget.SelectorWithWidgetPreference;
 
 import java.util.LinkedHashMap;
@@ -46,6 +47,9 @@ public class UsbDetailsFunctionsController extends UsbDetailsController
 
     private static final String TAG = "UsbFunctionsCtrl";
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+
+    private static final String USB_MODE_KEY_ALIAS = "usb_mode_auth_key";
+    private static final int MAX_UNLOCK_SECONDS = 60;
 
     static final Map<Long, Integer> FUNCTIONS_MAP = new LinkedHashMap<>();
 
@@ -199,7 +203,14 @@ public class UsbDetailsFunctionsController extends UsbDetailsController
     private boolean isAuthRequired(long function) {
         // Since webcam and MIDI don't transfer any persistent data over USB
         // don't require authentication.
-        return !(function == UsbManager.FUNCTION_UVC || function == UsbManager.FUNCTION_MIDI);
+        if (Flags.excludeWebcamAuthChallenge() &&
+                (function == UsbManager.FUNCTION_UVC || function == UsbManager.FUNCTION_MIDI)) {
+            return false;
+        }
+        if (WifiDppUtils.isUnlockedWithinSeconds(USB_MODE_KEY_ALIAS, MAX_UNLOCK_SECONDS)) {
+            return false;
+        }
+        return true;
     }
 
     private boolean isClickEventIgnored(long function, long previousFunction) {
