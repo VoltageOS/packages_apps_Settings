5d56dca340eeed1361a2cb44bdcf016c0e38ab48
NurKeinNeid <nurkeinneid@derpfest.org>
2025-05-04 21:48:49 +0200

    Settings: Enhance KeyboardNoNavigationBarController with command execution
    - Improve error handling while at it
    
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/navigationbar/KeyboardNoNavigationBarController.java
+++ src/com/android/settings/navigationbar/KeyboardNoNavigationBarController.java
@@ -19,6 +19,8 @@ public class KeyboardNoNavigationBarController extends BasePreferenceController 
 
     private static final String KEY_KEYBOARD_NO_NAVIGATION_BAR = "keyboard_no_navigation_bar";
     private static final String TAG = "NoNavbarController";
+    private static final String OVERLAY_PACKAGE = "com.voltage.overlay.customization.keyboard.nonavbar";
+    private static final String PROP_KEY = "persist.sys.keyboard_no_navigation_bar";
 
     public KeyboardNoNavigationBarController(Context context, String preferenceKey) {
         super(context, preferenceKey);
@@ -26,49 +28,75 @@ public class KeyboardNoNavigationBarController extends BasePreferenceController 
 
     @Override
     public int getAvailabilityStatus() {
-        int navigationMode = Settings.Secure.getInt(
-            mContext.getContentResolver(),
-            Settings.Secure.NAVIGATION_MODE,
-            0 // Default to 0 if the setting is not found
-        );
-
-        return navigationMode == 2
-                ? AVAILABLE 
-                : UNSUPPORTED_ON_DEVICE;
-
+        try {
+            int navigationMode = Settings.Secure.getInt(
+                mContext.getContentResolver(),
+                Settings.Secure.NAVIGATION_MODE,
+                0
+            );
+            return navigationMode == 2 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+        } catch (Exception e) {
+            Log.e(TAG, "Error getting navigation mode", e);
+            return UNSUPPORTED_ON_DEVICE;
+        }
     }
 
     public boolean isChecked() {
-        String value = SystemProperties.get("persist.sys.keyboard_no_navigation_bar", "1"); 
-        boolean result = "1".equals(value); // switch is ON only when overlay is disabled
-        Log.d(TAG, "isChecked() reversed logic returning: " + result);
-        return result;
+        try {
+            String value = SystemProperties.get(PROP_KEY, "1");
+            boolean result = "1".equals(value);
+            Log.d(TAG, "isChecked() returning: " + result);
+            return result;
+        } catch (Exception e) {
+            Log.e(TAG, "Error checking system property", e);
+            return true; // Default to true on error
+        }
+    }
+
+    private boolean executeOverlayCommand(boolean enable) {
+        String command = enable
+            ? "cmd overlay disable " + OVERLAY_PACKAGE
+            : "cmd overlay enable " + OVERLAY_PACKAGE;
+
+        try {
+            Process process = Runtime.getRuntime().exec(new String[]{"sh", "-c", command});
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                Log.e(TAG, "Overlay command failed with exit code: " + exitCode);
+                return false;
+            }
+            Log.d(TAG, "Successfully executed overlay command: " + command);
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to execute overlay command", e);
+            return false;
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (!(newValue instanceof Boolean)) {
-            Log.e(TAG, "Unexpected value type for preference: " + newValue);
+            Log.e(TAG, "Invalid preference value type: " + newValue);
             return false;
         }
 
         boolean enabled = (Boolean) newValue;
         String newPropValue = enabled ? "1" : "0";
-        SystemProperties.set("persist.sys.keyboard_no_navigation_bar", newPropValue);
-        Log.d(TAG, "onPreferenceChange() set persist.sys.keyboard_no_navigation_bar to: " + newPropValue);
-
-        String command = enabled
-                ? "cmd overlay disable com.voltage.overlay.customization.keyboard.nonavbar"
-                : "cmd overlay enable com.voltage.overlay.customization.keyboard.nonavbar";
 
         try {
-            Runtime.getRuntime().exec(new String[]{"sh", "-c", command});
-            Log.d(TAG, "Executed command: " + command);
-        } catch (Exception e) {
-            Log.e(TAG, "Failed to execute overlay command", e);
-        }
+            SystemProperties.set(PROP_KEY, newPropValue);
+            Log.d(TAG, "Set system property to: " + newPropValue);
 
-        return true;
+            if (!executeOverlayCommand(enabled)) {
+                Log.e(TAG, "Failed to update overlay state");
+                return false;
+            }
+
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG, "Error updating preference", e);
+            return false;
+        }
     }
 
     @Override
@@ -76,7 +104,7 @@ public class KeyboardNoNavigationBarController extends BasePreferenceController 
         if (preference instanceof SwitchPreferenceCompat) {
             boolean checked = isChecked();
             ((SwitchPreferenceCompat) preference).setChecked(checked);
-            Log.d(TAG, "updateState() setting switch to: " + checked);
+            Log.d(TAG, "Updated switch state to: " + checked);
         }
     }
 }
