211dc8390ea470735437d10bc2edb43b9b89cb71
Pranav Vashi <neobuddy89@gmail.com>
2025-09-01 00:54:54 +0530

    Settings: Re-add stop tracking listener in gesture navigation
    Requires framework change:
    https://github.com/crdroidandroid/android_frameworks_base/commit/c8d5f0f68407e8fe0bf270de6f427c8860508ecc
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/gestures/GestureNavigationSettingsFragment.java
+++ src/com/android/settings/gestures/GestureNavigationSettingsFragment.java
@@ -231,7 +231,10 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
                 mIndicatorView.setIndicatorWidth(mCurrentRightWidth, false);
                 mIndicatorView.setIndicatorWidth(mCurrentLefttWidth, true);
             }
+            return true;
+        });
 
+        pref.setOnPreferenceChangeStopListener((p,v) -> {
             final float scale = mBackGestureInsetScales[(int) v];
             if (key == GESTURE_BACK_HEIGHT_KEY) {
                 mIndicatorView.setIndicatorWidth(0, false);
