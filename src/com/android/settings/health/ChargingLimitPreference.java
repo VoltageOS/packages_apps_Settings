d22f34c1efca93ebe4200adb0920fd87a22c5d73
Bruno Martins <bgcngm@gmail.com>
2025-11-23 13:05:32 +0000

    Settings: Convert charging limit preference to M3E
    Partially picked from https://github.com/LineageOS/android_packages_apps_LineageParts/commit/cc2d85d612d73eb0f8f249aa151dc9d1ec1c8bc1
    
    Change-Id: I44923dc8b3c6acfde7f012d0588df800ff58f8e8
    Signed-off-by: Frost <frost.github@proton.me>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/health/ChargingLimitPreference.java
+++ src/com/android/settings/health/ChargingLimitPreference.java
@@ -1,46 +1,36 @@
 /*
- * Copyright (C) 2023 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package com.android.settings.health;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.SeekBar;
+import android.view.View;
 import android.widget.TextView;
 
-import androidx.preference.Preference;
 import androidx.preference.PreferenceViewHolder;
 
 import com.android.internal.custom.health.HealthInterface;
-
 import com.android.settings.R;
+import com.android.settingslib.widget.SliderPreference;
 
-public class ChargingLimitPreference extends Preference
-        implements SeekBar.OnSeekBarChangeListener {
+import com.google.android.material.slider.LabelFormatter;
+import com.google.android.material.slider.Slider;
+
+public class ChargingLimitPreference extends SliderPreference
+        implements Slider.OnSliderTouchListener {
     private static final String TAG = ChargingLimitPreference.class.getSimpleName();
 
+    private Slider mSlider;
+
     private TextView mChargingLimitValue;
-    private SeekBar mChargingLimitBar;
 
     private final HealthInterface mHealthInterface;
 
     public ChargingLimitPreference(final Context context, final AttributeSet attrs) {
         super(context, attrs);
-
-        setLayoutResource(R.layout.preference_charging_limit);
 
         mHealthInterface = HealthInterface.getInstance(context);
     }
@@ -49,34 +39,37 @@ public class ChargingLimitPreference extends Preference
     public void onBindViewHolder(final PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
 
-        mChargingLimitValue = (TextView) holder.findViewById(R.id.value);
+        mChargingLimitValue = (TextView) holder.findViewById(android.R.id.summary);
+        mChargingLimitValue.setVisibility(View.VISIBLE);
 
-        mChargingLimitBar = (SeekBar) holder.findViewById(R.id.seekbar_widget);
-        mChargingLimitBar.setOnSeekBarChangeListener(this);
+        mSlider = (Slider) holder.findViewById(com.android.settingslib.widget.preference.slider.R.id.slider);
+        mSlider.addOnSliderTouchListener(this);
+        mSlider.setLabelBehavior(LabelFormatter.LABEL_FLOATING);
+        mSlider.setStepSize(1);
+        mSlider.setTickVisible(false);
+        mSlider.setValueFrom(70);
+        mSlider.setValueTo(100);
 
         int currLimit = getSetting();
-        mChargingLimitBar.setProgress(currLimit);
+        mSlider.setValue(currLimit);
         updateValue(currLimit);
     }
 
     @Override
-    public void onStartTrackingTouch(final SeekBar seekBar) {
+    public void onStartTrackingTouch(final Slider slider) {
     }
 
     @Override
-    public void onStopTrackingTouch(final SeekBar seekBar) {
-        setSetting(seekBar.getProgress());
-    }
+    public void onStopTrackingTouch(final Slider slider) {
+        final int newLimit = (int) slider.getValue();
 
-    @Override
-    public void onProgressChanged(final SeekBar seekBar, final int progress,
-            final boolean fromUser) {
-        updateValue(progress);
+        setSetting(newLimit);
+        updateValue(newLimit);
     }
 
     public void setValue(final int value) {
-        if (mChargingLimitBar != null) {
-            mChargingLimitBar.setProgress(value);
+        if (mSlider != null) {
+            mSlider.setValue(value);
         }
         updateValue(value);
     }
@@ -90,8 +83,9 @@ public class ChargingLimitPreference extends Preference
     }
 
     private void updateValue(final int value) {
-        if (mChargingLimitValue != null) {
-            mChargingLimitValue.setText(String.format("%d%%", value));
+        if (value > 0 && mChargingLimitValue != null) {
+            mChargingLimitValue.setText(
+                    getContext().getString(R.string.charging_control_limit_summary, value));
         }
     }
 }
