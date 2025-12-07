BlissRoms/platform_packages_apps_Settings

[SQUASH] Settings: Redesign About Phone Page
Settings: Redesign about phone

Change-Id: I50ecd3aa03f19ee02e1f827d4bf8f2148ea9a309
Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

Settings: Introduce wallpaperBlur widget

Change-Id: Iddc75cd3ac3373b5ba376de0956f28611916ccf9
Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

Settings: Link ota card to updater

Change-Id: I6a04f19b64873a64dfff39709b45b1f64fb6e28c
Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

Settings: Introduce a nice shrinking effect while pressing on the ota card

Change-Id: If192e88ed22ff8c66a19a226b373930265d9203b
Co-authored-by: Jis G Jacob <studiokeys@blissroms.org>
Co-authored-by: redducc <sushmit718@gmail.com>

src/com/bliss/widget/DeviceNameTextView.java
@@ -1,0 +1,33 @@
+package com.bliss.widget;
+
+import android.content.Context;
+import android.os.Build;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class DeviceNameTextView extends AppCompatTextView {
+
+    public DeviceNameTextView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public DeviceNameTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public DeviceNameTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        String deviceName = Settings.Global.getString(context.getContentResolver(), Settings.Global.DEVICE_NAME);
+        if (deviceName == null) {
+            deviceName = Build.MODEL;
+        }
+        setText(deviceName);
+    }
+}

src/com/bliss/widget/ShrinkablePreference.java
@@ -1,0 +1,101 @@
+/*
+ * Copyright (C) 2024 PenguinOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bliss.widget;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+import com.android.settings.R;
+
+public class ShrinkablePreference extends Preference {
+
+    public ShrinkablePreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setWidgetLayoutResource(R.layout.bliss_tp_view);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+
+        View itemView = holder.itemView;
+        itemView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        ScaleAnimation shrinkAnimation = new ScaleAnimation(
+                            1f, 0.95f,
+                            1f, 0.95f,
+                            ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+                            ScaleAnimation.RELATIVE_TO_SELF, 0.5f);
+                        shrinkAnimation.setFillAfter(true);
+                        shrinkAnimation.setDuration(100);
+                        v.startAnimation(shrinkAnimation);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        ScaleAnimation expandAnimation = new ScaleAnimation(
+                            0.95f, 1.05f,
+                            0.95f, 1.05f,
+                            ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+                            ScaleAnimation.RELATIVE_TO_SELF, 0.5f);
+                        expandAnimation.setDuration(100);
+                        expandAnimation.setFillAfter(false);
+
+                        ScaleAnimation originalSizeAnimation = new ScaleAnimation(
+                            1.05f, 1f,
+                            1.05f, 1f,
+                            ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+                            ScaleAnimation.RELATIVE_TO_SELF, 0.5f);
+                        originalSizeAnimation.setDuration(100);
+                        originalSizeAnimation.setFillAfter(true);
+
+                        expandAnimation.setAnimationListener(new Animation.AnimationListener() {
+                            @Override
+                            public void onAnimationStart(Animation animation) {}
+
+                            @Override
+                            public void onAnimationEnd(Animation animation) {
+                                v.startAnimation(originalSizeAnimation);
+                            }
+
+                            @Override
+                            public void onAnimationRepeat(Animation animation) {}
+                        });
+
+                        v.startAnimation(expandAnimation);
+
+                        if (event.getAction() == MotionEvent.ACTION_UP) {
+                            Intent intent = new Intent("android.settings.SYSTEM_UPDATE_SETTINGS");
+                            getContext().startActivity(intent);
+                        }
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+}
+

src/com/bliss/widget/WallpaperBlurView.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2021 Project Radiant
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bliss.widget;
+
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.graphics.RenderEffect;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+
+public class WallpaperBlurView extends ImageView {
+
+    Context contextM;
+
+    public WallpaperBlurView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(18, 18, Shader.TileMode.CLAMP));
+    }
+
+    public WallpaperBlurView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(18, 18, Shader.TileMode.CLAMP));
+    }
+
+
+    public WallpaperBlurView(Context context) {
+        super(context);
+        contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(18, 18, Shader.TileMode.CLAMP));
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(contextM);
+        setImageDrawable(wallpaperManager.getDrawable());
+    }
+
+}

VoltageOS/packages_apps_Settings

Signed-off-by: Dmitrii <bankersenator@gmail.com>

src/com/voltage/widget/DeviceNameTextView.java
@@ -1,0 +1,33 @@
+package com.voltage.widget;
+
+import android.content.Context;
+import android.os.Build;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class DeviceNameTextView extends AppCompatTextView {
+
+    public DeviceNameTextView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public DeviceNameTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public DeviceNameTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        String deviceName = Settings.Global.getString(context.getContentResolver(), Settings.Global.DEVICE_NAME);
+        if (deviceName == null) {
+            deviceName = Build.MODEL;
+        }
+        setText(deviceName);
+    }
+}

src/com/voltage/widget/ShrinkablePreference.java
@@ -1,0 +1,109 @@
+/*
+ * Copyright (C) 2024 PenguinOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.voltage.widget;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+import com.android.settings.R;
+
+public class ShrinkablePreference extends Preference {
+
+    public ShrinkablePreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setWidgetLayoutResource(R.layout.voltage_tp_view);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+
+        View itemView = holder.itemView;
+        itemView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        ScaleAnimation shrinkAnimation = new ScaleAnimation(
+                            1f, 0.95f,
+                            1f, 0.95f,
+                            ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+                            ScaleAnimation.RELATIVE_TO_SELF, 0.5f);
+                        shrinkAnimation.setFillAfter(true);
+                        shrinkAnimation.setDuration(100);
+                        v.startAnimation(shrinkAnimation);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        ScaleAnimation expandAnimation = new ScaleAnimation(
+                            0.95f, 1.05f,
+                            0.95f, 1.05f,
+                            ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+                            ScaleAnimation.RELATIVE_TO_SELF, 0.5f);
+                        expandAnimation.setDuration(100);
+                        expandAnimation.setFillAfter(false);
+
+                        ScaleAnimation originalSizeAnimation = new ScaleAnimation(
+                            1.05f, 1f,
+                            1.05f, 1f,
+                            ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+                            ScaleAnimation.RELATIVE_TO_SELF, 0.5f);
+                        originalSizeAnimation.setDuration(100);
+                        originalSizeAnimation.setFillAfter(true);
+
+                        expandAnimation.setAnimationListener(new Animation.AnimationListener() {
+                            @Override
+                            public void onAnimationStart(Animation animation) {}
+
+                            @Override
+                            public void onAnimationEnd(Animation animation) {
+                                v.startAnimation(originalSizeAnimation);
+                            }
+
+                            @Override
+                            public void onAnimationRepeat(Animation animation) {}
+                        });
+
+                        v.startAnimation(expandAnimation);
+
+                        if (event.getAction() == MotionEvent.ACTION_UP) {
+                            Intent intent = new Intent("android.settings.SYSTEM_UPDATE_SETTINGS");
+                            try {
+                                getContext().startActivity(intent);
+                            } catch (ActivityNotFoundException e) {
+                                Toast.makeText(getContext(), 
+                                    "Apply for official to get updates",
+                                    Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+}
+

src/com/voltage/widget/WallpaperBlurView.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2021 Project Radiant
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.voltage.widget;
+
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.graphics.RenderEffect;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+
+public class WallpaperBlurView extends ImageView {
+
+    Context contextM;
+
+    public WallpaperBlurView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(18, 18, Shader.TileMode.CLAMP));
+    }
+
+    public WallpaperBlurView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(18, 18, Shader.TileMode.CLAMP));
+    }
+
+
+    public WallpaperBlurView(Context context) {
+        super(context);
+        contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(18, 18, Shader.TileMode.CLAMP));
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(contextM);
+        setImageDrawable(wallpaperManager.getDrawable());
+    }
+
+}