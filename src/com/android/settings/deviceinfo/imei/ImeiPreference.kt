8da4d186c49661bdaccdb983084df4791d3ecf0c
Frost <frost.github@proton.me>
2026-01-06 10:28:58 +0000

    Revert "Fix crash issues in About Phone settings"
    This reverts commit 7eb68f45a95c615e86d0fa377f08bd09a04673ab.
    
    Change-Id: I5a72309310ea7c636a7ecd75f26049b13a203b27
    Signed-off-by: Frost <frost.github@proton.me>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/deviceinfo/imei/ImeiPreference.kt
+++ src/com/android/settings/deviceinfo/imei/ImeiPreference.kt
@@ -84,15 +84,10 @@ class ImeiPreference(
         if (activeModemCount <= 1) {
             getString(R.string.status_imei)
         } else {
-            try {
-                val titleId =
-                    if (imei == telephonyManager?.primaryImei) R.string.imei_multi_sim_primary
-                    else R.string.imei_multi_sim
-                getString(titleId, slotIndex + 1)
-            } catch (exception: Exception) {
-                Log.e(TAG, "PrimaryImei not available.", exception)
-                getString(R.string.imei_multi_sim, slotIndex + 1)
-            }
+            val isPrimary = imei == telephonyManager?.primaryImei
+            val titleId =
+                if (isPrimary) R.string.imei_multi_sim_primary else R.string.imei_multi_sim
+            getString(titleId, slotIndex + 1)
         }
 
     companion object {
--- tests/unit/src/com/android/settings/deviceinfo/imei/ImeiPreferenceTest.kt
+++ tests/unit/src/com/android/settings/deviceinfo/imei/ImeiPreferenceTest.kt
@@ -26,7 +26,6 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.kotlin.doReturn
-import org.mockito.kotlin.doThrow
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.stub
 
@@ -59,15 +58,6 @@ class ImeiPreferenceTest {
             on { getImei(1) } doReturn IMEI_2
         }
         preference = ImeiPreference(context, 0, 1)
-    }
-
-    @Test
-    fun init_getPrimaryImeiThrowException_doNotCrash() {
-        mockTelephonyManager.stub {
-            on { primaryImei } doThrow(IllegalStateException())
-        }
-
-        preference = ImeiPreference(context, 0, 2)
     }
 
     @Test
